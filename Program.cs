5758bda809dffd4fa7f3423bfd20d74696be01a0
WIN_NT\x16j052 <x16j052@fjb-ac.jp>
2017-05-12 11:30:01 +0900

    no message

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace Example6
     {
         static void Main(string[] args)
         {
+            //変数の宣言
+            int score;
+            double pai;
+            String name;
+
+            score = 25;
+            pai = 3.1415;
+            name="TARO";
+
+            Console.WriteLine("SCORE ={0}", score);
+            Console.WriteLine("AVE ={0}", pai);
+            Console.WriteLine("NAME ={0}", name);
         }
     }
 }
